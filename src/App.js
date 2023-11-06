2a88f0ee78ca99e64be3867eb305690f2b059b1a
HyunSu1210 <hyunsu015@gmail.com>
2023-11-06 14:10:55 +0900

    231106-현수

--- src/App.js
+++ src/App.js
@@ -9,22 +9,22 @@ import UserMyPage from "./pages/MyPage.jsx/UserMyPage";
 import { StyledBackground } from "./components/Styles";
 import CompanyMyPage from "./pages/MyPage.jsx/CompanyMyPage";
 import EditCompanyMain from "./pages/MyPage.jsx/EditCompany";
+import GlobalStyle from "./style/globalStyle";
 // import JobListings
 
 function App() {
   return (
     <>
-      <StyledBackground>
-        <Router>
-          <Routes>
-            <Route path="/" element={<CompanyMyPage />} />
-            <Route path="/EditCompanyMain" element={<EditCompanyMain />} />
-            {/* <Route path="/JobListings" element={<JobListings />} /> */}
-          </Routes>
-        </Router>
+      <GlobalStyle />
+      <Router>
+        <Routes>
+          <Route path="/" element={<CompanyMyPage />} />
+          <Route path="/EditCompanyMain" element={<EditCompanyMain />} />
+          {/* <Route path="/JobListings" element={<JobListings />} /> */}
+        </Routes>
+      </Router>
 
-        {/* <JobListings /> */}
-      </StyledBackground>
+      {/* <JobListings /> */}
     </>
   );
 }
