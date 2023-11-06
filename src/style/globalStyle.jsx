c4ebc2b43800ec5aa6e049d1ae4de34a48a63f8c
HyunSu1210 <hyunsu015@gmail.com>
2023-11-06 14:37:04 +0900

    globalstyle 수정

--- src/style/globalStyle.jsx
+++ src/style/globalStyle.jsx
@@ -2,16 +2,15 @@ import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 
 const GlobalStyle = createGlobalStyle`
-${reset}
-:root {
-  --BLACK : #333;
-}
+  ${reset}
+  :root {
+    --BLACK : #333;
+  }
 
-* {
-  box-sizing: border-box;
-}
-
-html, body, div, span, applet, object, iframe,
+  * {
+    box-sizing: border-box;
+  }
+  html, body, div, span, applet, object, iframe,
   h1, h2, h3, h4, h5, h6, p, blockquote, pre,
   a, abbr, acronym, address, big, cite, code,
   del, dfn, em, img, ins, kbd, q, s, samp,
@@ -25,7 +24,28 @@ html, body, div, span, applet, object, iframe,
   menu, nav, output, ruby, section, summary,
   time, mark, audio, video{
     font-family: 'Noto Sans KR', sans-serif;
-    color: #333;
+  font-family: 'Orbit', sans-serif;
+  }
+  h2 {
+    font-size: 2.4em;
+  }
+  h3 {
+    font-size: 1.6em;
+  }
+  .container {
+    max-width: 1200px;
+    margin: 0 auto;
+  }
+  @media only screen and (max-width:768px) {
+    body {
+      font-size: 14px;
+    }
+    h2 {
+      font-size: 2em;
+    }
+    .container {
+      width: 90vw;
+    }
   }
 `;
 export default GlobalStyle;
