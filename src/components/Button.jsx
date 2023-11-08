e2af39f73d0eb2a739ec99480fee39a139105832
HyunSu1210 <hyunsu015@gmail.com>
2023-11-08 09:42:33 +0900

    버튼컴포넌트추가

---
+++ src/components/Button.jsx
@@ -0,0 +1,23 @@
+import styled, { css } from "styled-components";
+const TransBtn = styled.button`
+  border: 2px solid var(--RED);
+  background-color: transparent; // 버튼 배경색 없애기
+  color: var(--RED);
+  white-space: nowrap;
+  &:hover {
+    background-color: var(--RED);
+    color: white;
+  }
+
+  ${(props) => {
+    return css`
+      width: ${props.width};
+      height: ${props.height};
+      padding: ${props.padding};
+      margin: ${props.margin};
+      font-size: ${props.fontsize};
+    `;
+  }}
+`;
+
+export default TransBtn;
