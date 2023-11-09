41fde93223c535d71babc9942071365fd6f64cec
HanbiHeo <hhb0365@gmail.com>
2023-11-09 19:24:05 +0900

    board change

---
+++ src/api/BoardAxiosApi.jsx
@@ -0,0 +1,39 @@
+import axios from "axios";
+const DOBBY_DOMAIN = "http://localhost:8111";
+
+const BoardAxiosApi = {
+  //게시글 조회
+  boardList: async () => {
+    return await axios.get(DOBBY_DOMAIN + "/api/board");
+  },
+
+  // // 게시글 상세 조회
+  // boardDetail: async (boardId) => {
+  //   return await axios.get(DOBBY_DOMAIN + `/api/board/detail/${boardId}`);
+  // },
+  // // 게시글 쓰기
+  // boardWrite: async (title, content, userId, img) => {
+  //   const board = {
+  //     title: title,
+  //     content: content,
+  //     userId: userId,
+  //     img: img,
+  //   };
+  //   return await axios.post(DOBBY_DOMAIN + "/api/board/new", board);
+  // },
+  // // 게시글에 달린 댓글 조회
+  // commentList: async (boardId) => {
+  //   return await axios.get(DOBBY_DOMAIN + `/api/comment/list/${boardId}`);
+  // },
+  // // 댓글 쓰기
+  // commentWrite: async (userId, boardId, content) => {
+  //   const comment = {
+  //     boardId: boardId,
+  //     userId: userId,
+  //     content: content,
+  //   };
+  //   return await axios.post(DOBBY_DOMAIN + `/api/comment/new`, comment);
+  // },
+};
+
+export default BoardAxiosApi;
