3f09bb5c787172f2895234a3416289d6ce67b628
HyunSu1210 <hyunsu015@gmail.com>
2023-11-11 01:25:47 +0900

    변수명 카멜 케이스로 바꿈

--- src/pages/MyPage/UserMyPage.jsx
+++ src/pages/MyPage/UserMyPage.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 import Profile from "../../components/Mypage/profile/Profile";
 
 // 제일 아래에 배치된 div
-const Profile_box = styled.div`
+const ProfileBox = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -48,13 +48,13 @@ const UserMyPage = () => {
   };
 
   return (
-    <Profile_box>
+    <ProfileBox>
       <Profile />
       <Btn onClick={EditNavigate}>내 정보 수정</Btn>
       <Btn onClick={PostNavigate}>작성글 보기</Btn>
       <Btn onClick={ApplyNavigate}>지원 회사 목록</Btn>
       <Btn>즐겨찾기 목록</Btn>
-    </Profile_box>
+    </ProfileBox>
   );
 };
 
